c318631225354d347efbeed67ac4c30198d0640b
M4C30 <maceosylvanbeltman@gmail.com>
2021-06-16 23:06:08 +0200

    php edit

--- php/verzend.php
+++ php/verzend.php
@@ -6,6 +6,11 @@
     {
 
         $email = $_POST['nieuws'];
+        $headers = array(
+            'From' => 'noreply@planco.com',
+            'Reply-To' => 'noreply@planco.com',
+            'X-Mailer' => 'PHP/' . phpversion()
+        );
         $bericht = "";
         $bericht .= "Automatische mail naar " . $email . "\r\n";
         $bericht .= "\r\n";
@@ -13,10 +18,13 @@
         $bericht .= "\r\n";
         $bericht .= "Je kunt op elk moment jouw abonnement afzeggen.";
         $bericht .= "\r\n";
+        $bericht .= "\r\n";
         $bericht .= "Veel geluk namens ons bij Planco!";
 
-        mail($email,"Nieuwsbrief Conformatie",$bericht);
+        
+        mail($email,"Nieuwsbrief Conformatie",$bericht,$headers);
 
+        echo '<script type="text/JavaScript">  window.history.back();   </script>';
     }
 
  
