d567cdd34e26a26968a5e578d5b0e9bb248a0e84
A6Dice <3352de@gmail.com>
2021-07-04 21:06:21 +0200

    KOK

--- javascript/FAQ.js
+++ javascript/FAQ.js
@@ -1,9 +1,9 @@
-var cc = document.getElementsByClassName('accordion');
+var acc = document.getElementsByClassName('accordion');
 var i;
 var len = acc.length;
 for(i = 0; i < len; i++){
     acc[i].addEventListener('click', function(){
-        this.classlist.toggle('active');
+        this.classList.toggle('active1');
         var panel = this.nextElementSibling;
         if(panel.style.maxHeight) {
             panel.style.maxHeight = null;
